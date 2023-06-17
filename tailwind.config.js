1a3632dd4e25f3090f62bd92cda7031eed104be2
Sid Ahmed Sahraoui <s.sahraoui@esi-sba.dz>
2023-06-17 20:23:38 +0100

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,109 +1,76 @@
-const windmill = require("@windmill/react-ui/config");
-const colors = require("tailwindcss/colors");
+const { fontFamily } = require("tailwindcss/defaultTheme");
 
-module.exports = windmill({
-  mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
-  mode: "jit",
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: ["app/**/*.{ts,tsx}", "components/**/*.{ts,tsx}"],
   theme: {
-    colors: {
-      rose: colors.rose,
-      fuchsia: colors.fuchsia,
-      indigo: colors.indigo,
-      slate: colors.slate,
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
     },
     extend: {
-      fontFamily: {
-        lato: ["lato", "sans-serif"],
-        sans: [
-          "lato",
-          "BlinkMacSystemFont",
-          "-apple-system",
-          "Segoe UI",
-          "Roboto",
-          "Oxygen",
-          "Ubuntu",
-          "Cantarell",
-          "Fira Sans",
-          "Droid Sans",
-          "Helvetica Neue",
-          "Helvetica",
-          "Arial",
-          "sans-serif",
-        ],
-        mono: ["Source Code Pro", "Menlo", "monospace"],
-      },
-
       colors: {
-        gray: {
-          100: "#FFFFFF",
-          150: "#3f4046",
-          200: "#EFEFEF",
-          300: "#DADADA",
-          400: "#818181",
-          500: "#6F767E",
-          600: "#404B53",
-          650: "#202427",
-          700: "#232830", //"#26282C", //"#2B3034",
-          750: "#1A1C22",
-          800: "#050A0E",
-          850: "#26282C",
-          900: "#95959E",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
         },
-        orange: {
-          250: "#FF5810",
-          350: "#FF5D5D",
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
         },
-        yellow: {
-          150: "#FF900C",
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
         },
-        purple: {
-          350: "#5568FE",
-          550: "#596BFF",
-          600: "#586FEA",
-          650: "#2B3480",
-          700: "#4F63D2",
-          750: "#6246FB",
-          300: "#4658BB",
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
         },
-        red: { 150: "#D32F2F", 650: "#F44339" },
-        pink: {
-          150: "#EC4899",
-          250: "#FFB5B5",
-          750: "#2c1a22",
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
         },
-        green: {
-          150: "#3BA55D",
-          250: "#40A954",
-          350: "#34A85333",
-          450: "#34A85380",
-          550: "#87E5A2",
-          650: "#96F3D24D",
-          750: "#A3FEE3",
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
         },
-        blue: {
-          350: "#76d9e6",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
         },
-        customGray: {
-          100: "#252A34",
-          150: "#31353B",
-          200: "#1E1E1E",
-          300: "#454545",
-          350: "#2B303499",
-          400: "#282828",
-          500: "#848484",
-          600: "#C4C4C4",
-          700: "#272727",
-          800: "#343434",
-          850: "#9E9DA6",
-          900: "#373C43",
+      },
+      borderRadius: {
+        lg: `var(--radius)`,
+        md: `calc(var(--radius) - 2px)`,
+        sm: "calc(var(--radius) - 4px)",
+      },
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
         },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/typography")],
-});
+  plugins: [require("tailwindcss-animate")],
+};
